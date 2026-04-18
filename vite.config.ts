4329e6cda1bf87b6fe2364615b72126613970337
Julia Fokanova <oncologmma@mail.ru>
2026-04-18 18:32:06 +0300

    chore: rename project

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/RA_router-menu',
+  base: '/menu-router',
   server: {
     open: true,
   },
